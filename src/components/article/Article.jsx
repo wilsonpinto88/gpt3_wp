43040fe85b78841290ffdea2a07ad5c10199bb4e
Wilson Pinto <wilsonpinto88@gmail.com>
2023-03-01 15:13:46 +0000

    added the CTA, blog and footer sections

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <Fragment>
+      <div className="gpt3__blog-container_article">
+        <div className="gpt3__blog-container_article-image">
+          <img src={imgUrl} alt="blog" />
+        </div>
+        <div className="gpt3__blog-container_article-content">
+          <div>
+            <p>{date}</p>
+            <h3>{title}</h3>
+            <p>Read Full Article</p>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Article;
--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <Fragment>
+      <div className="gpt3__cta">
+        <div className="gpt3__cta-content">
+          <p>Request Early Access to Get Started</p>
+          <h3>Register today & start exploring the endless possiblities.</h3>
+        </div>
+        <div className="gpt3__cta-btn">
+          <button type="button">Get Started</button>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default CTA;
